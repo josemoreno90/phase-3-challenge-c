7c2e39235693b1d6afc514bbd3f997597ef5a220
Jose Moreno <morenojr90@yahoo.com>
2017-10-23 16:08:53 -0700

    Finish part-1-route-2, Start on databse schema, and hotel.js, add csv files

---
+++ part-2/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS hotel_db;
+CREATE DATABASE hotel_db;
+
+\c hotel_db;
+
+CREATE TABLE guests(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(250),
+  email VARCHAR(250)
+);
+
+CREATE TABLE rooms(
+  id SERIAL PRIMARY KEY,
+  room_number VARCHAR(250),
+  capacity INTEGER,
+  available BOOLEAN DEFAULT true
+);
+
+CREATE TABLE bookings(
+  id SERIAL PRIMARY KEY,
+  room_id INTEGER REFERENCES rooms (id),
+  guest_id INTEGER REFERENCES guests (id),
+  check_in DATE,
+  check_out DATE
+);
