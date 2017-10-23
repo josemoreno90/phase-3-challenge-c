ef8bdf326d5cafb2e80f664ee859c64f04324e68
Jose Moreno <morenojr90@yahoo.com>
2017-10-23 13:22:59 -0700

    Finish route 1, URL parameter :word is returned in all caps with 3 exclamation points

--- part-1/server.js
+++ part-1/server.js
@@ -5,4 +5,9 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 
+app.get('/api/shout/:word', (req, res) => {
+  const word = req.params.word.toUpperCase();
+  res.send(word + "!!!");
+})
+
 app.listen(3000, () => console.log('We hear you'));
